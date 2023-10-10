b8eed3877505390397d324b949fb6281159cfc41
ciannella <lucasciannella@hotmail.com>
2023-10-10 02:07:26 -0300

    refact: refactoring code from Vehicle Test controller, adding disable annotation

--- src/test/java/com/icarros/vehiclesystem/controller/VehicleControllerTest.java
+++ src/test/java/com/icarros/vehiclesystem/controller/VehicleControllerTest.java
@@ -11,7 +11,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -19,7 +18,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//TODO:AJUSTAR TESTES QUANDO A API_FIPE VOLTAR A FUNCIONAR
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
